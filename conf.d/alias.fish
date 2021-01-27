fb1a17a6417bfa6c55fe1e4283d910950464ce3e
Danijel <danijel.se@gmail.com>
2021-01-27 21:07:49 +0100

    updated files

--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -40,3 +40,17 @@
 
 	# Add progressbar when copy files
 	    alias cpv='rsync -ah --info=progress2'
+
+# Some tmux-related shell aliases
+
+# Attaches tmux to the last session; creates a new session if none exists.
+alias t='tmux attach || tmux new-session'
+
+# Attaches tmux to a session (example: ta portal)
+alias ta='tmux attach -t'
+
+# Creates a new session
+alias tn='tmux new-session'
+
+# Lists all ongoing sessions
+alias tl='tmux list-sessions'
