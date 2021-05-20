0069be35ac5ea90cf4b5d0cc8f425896003f26ef
Danijel <danijel.se@gmail.com>
2021-05-20 08:48:40 +0200

    updateded files

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,12 +1,14 @@
 #code from Bobthefish theme
 function fish_greeting -d "What's up, fish?"
     clear
-    set_color $fish_color_autosuggestion
-    uname -nmsr
-
-    # TODO: `command -q -s` only works on fish 2.5+, so hold off on that for now
-    command -s uptime >/dev/null
+    #set_color $fish_color_autosuggestion
+    #uname -nmsr
+	
+    	# TODO: `command -q -s` only works on fish 2.5+, so hold off on that for now
+    #command -s uptime >/dev/null
     and command uptime
+
+	stats
 
     set_color normal
 end
