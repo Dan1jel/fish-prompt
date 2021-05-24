98822df76d7f405cce13f2c768fe4596d0662c71
Danijel <danijel.se@gmail.com>
2021-05-24 19:09:46 +0200

    updated files

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -5,10 +5,8 @@ function fish_greeting -d "What's up, fish?"
     #uname -nmsr
 	
     	# TODO: `command -q -s` only works on fish 2.5+, so hold off on that for now
-    #command -s uptime >/dev/null
-    and command uptime
-
-	stats
-
+    # command -s uptime >/dev/null
+    # and command uptime
+	stats br
     set_color normal
 end
