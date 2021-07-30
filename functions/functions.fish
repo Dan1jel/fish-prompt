fa44856f3893c4f51071b22971082b04d0995498
Danijel <danijel.se@gmail.com>
2021-07-30 17:57:08 +0200

    updated files

--- functions/functions.fish
+++ functions/functions.fish
@@ -1,9 +1,16 @@
+fish_add_path -P ~/.local/bin/
+
 function t-remove
 	transmission-remote -t $argv -rad
 end
+funcsave t-remove
 
 function find
 	fdfind $argv
 end
+funcsave find
 
-fish_add_path -P ~/.local/bin/
+function ac3-convert
+	ffmpeg -i $argv[1] -map 0:v -map 0:a:0 -map 0:s -c copy -c:a ac3 -b:a 640k $argv[2]
+end
+funcsave ac3-convert
