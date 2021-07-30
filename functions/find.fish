c649a1e48bd1f106aa6ead1b5a86c40e47f0d556
Danijel <Danijel.se@gmail.com>
2021-07-30 20:21:56 +0200

    updated files

--- functions/find.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /home/pi/.config/fish/functions/find.fish @ line 2
-function find
-	fdfind $argv
-end
--- functions/t-remove.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /home/pi/.config/fish/functions/t-remove.fish @ line 2
-function t-remove
-	transmission-remote -t $argv -rad
-end
