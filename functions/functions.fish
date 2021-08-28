8abfe426d54b4be751655f5cf269946396958b13
Danijel <danijel.se@gmail.com>
2021-08-28 08:03:57 +0200

    updated files

--- functions/functions.fish
+++ functions/functions.fish
@@ -11,3 +11,7 @@ end
 function ac3
 	ffmpeg -i $argv[1] -map 0:v -map 0:a:0 -map 0:s -c copy -c:a ac3 -b:a 640k $argv[2]
 end
+
+funcion hardsub
+	ffmpeg -i $argv[1] -c:a copy c:v copy -vf "subtitles=$argv[1]:si=0" /server/media/'out.mp4'
+end
