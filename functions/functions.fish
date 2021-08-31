e36e9e4a05b1edd897cfdd63fe80aac26c8d3f4c
Danijel <danijel.se@gmail.com>
2021-08-31 22:33:03 +0200

    updated files

--- functions/functions.fish
+++ functions/functions.fish
@@ -14,6 +14,9 @@ end
 
 function hardsub
   set input_filename $argv[1]
-  set output_filename (echo $input_filename | sed 's/\.[^.]*$//')
+  set output_filename (echo $input | sed 's/\.[^.]*$//')
   ffmpeg -i "$input_filename" -filter_complex "subtitles='$input_filename':si=0" -c:a copy "$output_filename".mp4
+end
+
+for i in *.mkv; ffmpeg -i "$i" -filter_complex "subtitles='$i':si=0" -c:a copy (string split -r -m1 . $i)[1]".mp4";
 end
