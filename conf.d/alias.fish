1367e601d703ddc9c2871e344b3b982e3bb37209
Danijel <danijel.se@gmail.com>
2021-08-31 16:27:39 +0200

    updated files

--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -125,10 +125,10 @@
 	    alias full-backup='~/.dotfiles/scripts/./tmux_backup.sh'
 
 	# Hardencode subs with embeded subs within mkv.
-	    alias hardsub-mkv='~/.dotfiles/scripts/./hardsub-mkv.sh'
+	    alias hardsub-mkv='~/termux-backup/good_scripts/./hardsub-mkv.sh'
 
 	# Hardencode subs with .ass files and .mp4.
-	    alias hardsub-ass='~/.dotfiles/scripts/./hardsub-ass.sh'
+	    alias hardsub-ass='~/termux-backup/good_scripts/./hardsub-ass.sh'
 	    
 	# Hardencode subs with embeded subs within mkv.
 	    #alias hardsub-mkv='for i in *.mkv; do ffmpeg -i "$i" -filter_complex "subtitles='$i':si=0" -c:a copy "$i%.*.mp4"; done'
