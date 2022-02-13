34bddd7c5ee8f4efd8bd2926833d9c79fcf0fd62
Danijel <danijel.se@gmail.com>
2022-02-13 10:43:58 +0100

    update

--- functions/functions.fish
+++ functions/functions.fish
@@ -2,96 +2,95 @@ fish_add_path -P ~/.local/bin/
 
 function t-remove
 	transmission-remote -n 'Dan1jel:Paras1t3' -t $argv -r
-end
+	end
 
 function t-remove-all
 	transmission-remote -n 'Dan1jel:Paras1t3' -t $argv -rad
-end
+	end
 
 function t-start
 	transmission-remote -n 'Dan1jel:Paras1t3' -t $argv -s
-end
+	end
 
 function t-stop
 	transmission-remote -n 'Dan1jel:Paras1t3' -t $argv -S
-end
+	end
 
 function ac3
 	#ffmpeg -i $argv[1] -map 0:v -map 0:a:0 -map 0:s -c copy -c:a ac3 -b:a 640k $argv[2]
 	ffmpeg -i $argv[1] -map 0:v -map 0:a:0 -c copy -c:a ac3 -b:a 640k $argv[2]
-end
+	end
 
 function stats 
 	cat ~/termux-backup/ascii_art/ascii_$argv[1]
-end
-
-  # use bat instead of cat
-function cat 		# use bat instead of cat
-	if command -q bat
-	  command bat --style=plain $argv
-	else
-	  command cat $argv
 	end
-end
+
+function cat 		# use bat instead of cat
+	 if command -q bat
+	  command bat --style=plain $argv
+	 else
+	  command cat $argv
+	 end
+	end
 
 function find 		# Replace find with fdfind (fd on termux)
-	if command -q fd
+	 if command -q fd
 	  command fd $argv
-	else if command -q fdfind
+	 else if command -q fdfind
 	  command fdfind $argv
-	else
+	 else
 	  command find $argv
+	 end
 	end
-end
 
 function l		# Replace 'ls' with 'exa' (if available) + some aliases.
-	if command -q exa
+	 if command -q exa
 	  command exa $argv
-	else
+	 else
 	  command ls -G $argv
+	 end
 	end
-end
 
 function ls
-        if command -q exa
+         if command -q exa
 	  command exa -a $argv
-	else
+	 else
 	  command ls -G $argv
+	 end
 	end
-end
 
 function l. 
-	if command -q exa 
+	 if command -q exa 
 	  command exa -d .* $argv
-	else
+	 else
 	  command ls -d .* $argv
+	 end
 	end
-end
 
 function la 
-	if command -q exa 
+	 if command -q exa 
 	  command exa -la $argv
-	else
+	 else
 	  command ls -a $argv
+	 end
 	end
-end
 
 function ll
-	if command -q exa 
+	 if command -q exa 
 	  command exa -Fhl $argv
-	else
+	 else
 	  command ls -Fhl $argv
+	 end
 	end
-end
 
 function ll.
-	if command -q exa 
+	 if command -q exa 
 	  command exa -Fhl -d .* $argv
-	else
+	 else
 	  command ls -Fhl -d .* $argv
+	 end
 	end
-end
 
 function tree
 	command exa --tree $argv
-end
+	end
