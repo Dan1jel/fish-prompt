5f76bfc02f8e7431458cb3d07f5704238b715000
Danijel <Danijel.se@gmail.com>
2022-03-08 10:26:46 +0100

    update files

--- script/ntfy.sh
+++ script/ntfy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-bash -c "curl -H p:5 't:$(hostname) Terminal' -d 'Your command just finnished.' ntfy.sh/bejkon"
+bash -c "curl -H 't:$(hostname) Terminal' -d 'Your command just finnished.' ntfy.sh/bejkon"
