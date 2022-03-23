f16b984158a567c42d2b0e0caa9eba5faf8197ac
Danijel <danijel.se@gmail.com>
2022-03-23 19:53:38 +0100

    update files

--- config.fish
+++ config.fish
@@ -5,9 +5,9 @@ end
 set -Ux FZF_DEFAULT_OPTS '--height 40% --border --reverse --color=fg:#f8f8f2,bg:#000000,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#282a36,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
 
 if command -q bat
-    set -Ux FZF_CTRL_T_OPTS "--preview 'bat -p --color=always {}'"
+    set -Ux FZF_CTRL_T_OPTS "--preview 'bat -p --color=always {}' --preview-window right:50%:hidden:wrap --bind '?:toggle-preview'"
 else if command -q batcat
-    set -Ux FZF_CTRL_T_OPTS "--preview 'batcat -p --color=always {}'"
+    set -Ux FZF_CTRL_T_OPTS "--preview 'batcat -p --color=always {}' --preview-window right:50%:hidden:wrap --bind '?:toggle-preview'"
 end
 
 if command -q fd
@@ -20,4 +20,6 @@ else if command -q fdfind
     set -Ux FZF_CTRL_T_COMMAND 'fdfind --type f --type d --hidden --follow --exclude .git'
 end
 
-set -Ux FZF_ALT_C_OPTS "--preview 'exa --tree {}'"
+set -Ux FZF_ALT_C_OPTS "--preview 'exa --tree {}' --preview-window right:50%:hidden:wrap --bind '?:toggle-preview'"
+
+set -Ux FZF_CTRL_R_OPTS "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
