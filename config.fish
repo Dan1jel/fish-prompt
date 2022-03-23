edc4b07e2687e7e7c004af27b27d0785f6054ac1
Danijel Wallenborg <danijel.se@gmail.com>
2022-03-23 14:06:03 +0100

    update files

--- config.fish
+++ config.fish
@@ -1,2 +1,8 @@
 set -x PATH $PATH ~/.local/bin
 set -gx EDITOR vim
+
+set -Ux FZF_DEFAULT_OPTS '--height 40% --border --reverse --color=fg:#f8f8f2,bg:#000000,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#282a36,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+
+set -Ux FZF_ALT_C_OPTS "--preview 'exa -tree {}'"
+
+set -Ux FZF_CTRL_T_OPTS "--preview 'batcat -p --color=always {}'"
---
+++ functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/danijel/fzf/shell/key-bindings.fish
