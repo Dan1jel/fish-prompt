80bbff9971b4538f76d5646bdd0a497c6e16900b
Danijel <Danijel.se@gmail.com>
2023-05-22 14:34:17 +0200

    updated NTFY script

--- script/ntfy.sh
+++ script/ntfy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 # bash -c "curl -H 't:$(hostname) Terminal' -d 'Your command just finnished.' ntfy.sh/bejkon"
 
+SECONDS=0
+
+sleep 5
+
+duration=$SECONDS
+
 bash -c "curl -H 't:$(date +"%H:%M") $(date +"%y/%m/%d") - $(hostname)' -d 'Your command just finnished.
 
-⚬ ffmpeg runtime: $((0 / 3600))h:$(((0 % 3600) / 60))m:$(((0 % 3600) % 60))s.
+⚬ ffmpeg runtime: $(($duration / 3600))h:$((($duration % 3600) / 60))m:$((($duration % 3600) % 60))s.
 > $(/bin/ls -Art | tail -n 1)' ntfy.sh/bejkon"
